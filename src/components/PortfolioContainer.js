129ff7617a2a2707f5642de24c61a008617f350a
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-05-16 20:51:34 -0400

    stock exchange sort and remove

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({stocks, onRemoveStock}) {
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+       {
+        stocks.map((stock) => (
+      <Stock key={stock.id} stock={stock} onStockClick={onRemoveStock} />
+        ))}
+      
     </div>
   );
 }
